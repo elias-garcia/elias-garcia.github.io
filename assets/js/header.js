310b5cbff9a80ce5a492fa75a0199c065faea157
elias-garcia <elias.garcia@udc.es>
2017-03-16 00:45:46 +0100

    Fixed js menu script

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,10 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  var mainHeader = document.querySelector(".main-header h1");
-  var menuElements = document.querySelectorAll("header a");
+document.addEventListener('DOMContentLoaded', function(event) {
+  var path = window.location.pathname;
+  var menuElements = document.querySelectorAll('header a');
+  
+  if (path === '/' | path.includes('/blog')) {
+    menuElements[0].className += ' active';
+  }
 
-  menuElements.forEach(function(element) {
-    if (element.innerHTML.toLowerCase() === mainHeader.innerHTML.toLowerCase()) {
-      element.className += " active";
-    }
-  });
+  if (path === '/sobre-mi/') {
+    menuElements[1].className += ' active';
+  }
+
+  if (path === '/contacto/') {
+    menuElements[2].className += ' active';
+  }
+
 });
