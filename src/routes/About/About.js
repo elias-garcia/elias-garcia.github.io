0ef3e754b1a6ec594f2b2a6790f485830133d0a7
elias-garcia <eliasgarciadev@gmail.com>
2019-06-11 17:40:34 +0200

    displaying data in about component

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './About.css';
+import AboutData from '../../assets/data/about.json';
+
+const getAboutParagraphs = () => {
+return Object.keys(AboutData).map((key, i) => {
+  return <p key={i}>{AboutData[key]}</p>;
+})
+}
 
 const About = () => (
-  <div>
-    <p>About works</p>
+  <div className="About">
+    {getAboutParagraphs()}
   </div>
 );
 
