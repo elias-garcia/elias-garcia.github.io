352eb9cd0b484f0c7f5c60d92ef48725a7821f94
Elias Garcia <eliasgarciadev@gmail.com>
2022-05-26 22:18:35 +0200

    Updates

---
+++ precache-manifest.d5fc35883f575c4e5249041995217666.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70e1bf338efb0652bd41b00b370721f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0adeaaa0c95a06413db",
+    "url": "/static/css/main.4d1c2d1f.chunk.css"
+  },
+  {
+    "revision": "05ba2b45cea85dbef158",
+    "url": "/static/js/2.d34350cd.chunk.js"
+  },
+  {
+    "revision": "e0adeaaa0c95a06413db",
+    "url": "/static/js/main.cf2296a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1c0c349ad9921e0675e6d7a2920d4c72",
+    "url": "/static/media/angular-logo.1c0c349a.svg"
+  },
+  {
+    "revision": "9af2f104828cc13ca40c628e7e30e666",
+    "url": "/static/media/apperta.9af2f104.jpg"
+  },
+  {
+    "revision": "921a1f1bd73752fc9b9f3105686527d7",
+    "url": "/static/media/financial-manager.921a1f1b.jpg"
+  },
+  {
+    "revision": "dee70092badc6145039a2750390cad69",
+    "url": "/static/media/logo.dee70092.svg"
+  },
+  {
+    "revision": "5493187b40c9863bdaba547dc0613edb",
+    "url": "/static/media/me.5493187b.jpg"
+  },
+  {
+    "revision": "40854f0911900ec31b59221b4c686a3f",
+    "url": "/static/media/sneaker-tracker.40854f09.jpg"
+  },
+  {
+    "revision": "779d07a231d4a10b1d452d5784203b10",
+    "url": "/static/media/sporter.779d07a2.jpg"
+  }
+]);
